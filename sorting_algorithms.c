d91d1b29986e915cafd02b07e6d564166550c4f2
Lucas Ressler <luc.signin96@gmail.com>
2022-12-09 14:51:41 +0100

    modified command line interface slightly

--- sorting_algorithms.c
+++ sorting_algorithms.c
@@ -34,32 +34,32 @@ int string_eq(char *s1, char *s2)
 
 int main(int argc, char *args[])
 {
-    if (argc == 2)
+    if (argc >= 2)
     {
-        if (string_eq(args[1], "1.1"))
+        if (string_eq(args[1], "-1.1"))
         {
             execute_11();
         }
-        else if (string_eq(args[1], "1.2"))
+        else if (string_eq(args[1], "-1.2"))
         {
             execute_12();
         }
-        else if (string_eq(args[1], "1.3"))
+        else if (string_eq(args[1], "-1.3"))
         {
             execute_13();
         }
-        else if (string_eq(args[1], "1.4"))
+        else if (string_eq(args[1], "-1.4"))
         {
             execute_14();
         }
         else
         {
-            fprintf(stderr, "invalid argument. valid arguments are \"1.1\", \"1.2\", \"1.3\", \"1.4\"");
+            fprintf(stderr, "invalid argument. valid arguments are \"-1.1\", \"-1.2\", \"-1.3\", \"-1.4\"");
         }
     }
     else
     {
-        fprintf(stderr, "invalid amount of arguments");
+        fprintf(stderr, "missing argument: provide one of \"-1.1\", \"-1.2\", \"-1.3\", \"-1.4\"");
     }
     return 0;
 }
