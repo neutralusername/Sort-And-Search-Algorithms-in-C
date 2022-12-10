16530846e8f626283cb0554b5566bb0820a87845
Lucas Ressler <luc.signin96@gmail.com>
2022-12-10 15:33:36 +0100

    marked mergesort as 'implementation missing'

--- mergesort.c
+++ mergesort.c
@@ -1,12 +1,4 @@
-#include <stdio.h>
-#include "array_functions.h"
-
-void mergeSortArray(int *array, int length, int ascending) {
-    printf("mergesort - implementation missing!\n\narray:\n");
-    printArray(array, length);
-    if(ascending) {
-        printf("\n\nascending");
-    } else {
-        printf("\n\ndescending");
-    }
+void mergeSortArray(int *array, int length, int ascending)
+{
+    // implementation missing
 }
