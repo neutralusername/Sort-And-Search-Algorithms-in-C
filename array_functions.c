49c3d7382473af4ade73c317d17205ab37d4b225
Lucas Ressler <luc.signin96@gmail.com>
2022-12-10 15:32:02 +0100

    changed unnecessary int functions into voids, 'fixed' the randomize function

--- array_functions.c
+++ array_functions.c
@@ -3,58 +3,72 @@
 #include <time.h>
 #include <math.h>
 
-int validateSortedArray(int *input_array, int length, int ascending) {
-    if(ascending)  //if the array should be sorted in ascending order
-        for(int i = 0; i < length - 1; i++) {
-            if(input_array[i] > input_array[i + 1]) { //if the next element is smaller than the current element
-                return 0; //the array is not sorted in ascending order and the function returns 0
+int validateSortedArray(int *input_array, int length, int ascending)
+{
+    if (ascending) // if the array should be sorted in ascending order
+        for (int i = 0; i < length - 1; i++)
+        {
+            if (input_array[i] > input_array[i + 1])
+            {             // if the next element is smaller than the current element
+                return 0; // the array is not sorted in ascending order and the function returns 0
             }
         }
-    else for(int i = 0; i < length - 1; i++) { //if the array should be sorted in descending order
-        if(input_array[i] < input_array[i + 1]) { //if the next element is bigger than the current element
-            return 0; //the array is not sorted in descending order and the function returns 0
+    else
+        for (int i = 0; i < length - 1; i++)
+        { // if the array should be sorted in descending order
+            if (input_array[i] < input_array[i + 1])
+            {             // if the next element is bigger than the current element
+                return 0; // the array is not sorted in descending order and the function returns 0
+            }
         }
-    }
     return 1;
 }
 
-void printArray(int *input_array, int length) {
-    int lineBreak = 0; //counter for linebreaks
-    for(int i = 0; i < length; i++) {
+void printArray(int *input_array, int length)
+{
+    int lineBreak = 0; // counter for linebreaks
+    for (int i = 0; i < length; i++)
+    {
         printf("%d ", input_array[i]);
-        if(++lineBreak > 14) { //print 15 numbers per line
+        if (++lineBreak > 14)
+        { // print 15 numbers per line
             printf("\n");
-            lineBreak = 0; //reset the counter
+            lineBreak = 0; // reset the counter
         }
     }
     printf("\n");
 }
 
-int randomizeArray(signed int *array, int length) { 
-    srand(time(0));
-    for (int i = 0; i < length; i++) {
+void randomizeArray(signed int *array, int length)
+{
+    for (int i = 0; i < length; i++)
+    {
         int randomint = rand();
-        if(rand() % 2) {
+        if (rand() % 2)
+        {
             randomint = randomint * -1;
             randomint -= 1;
         }
         *(array + i) = randomint;
     }
-    return 0;
 }
 
-void fillArrayAscening(signed int *array, int length) {
-    for(int i = 0; i < length; i++)
+void fillArrayAscening(signed int *array, int length)
+{
+    for (int i = 0; i < length; i++)
         *(array + i) = -32768 + i;
 }
 
-void fillArrayDescending(signed int *array, int length) {
-    for(int i = 0; i < length; i++)
+void fillArrayDescending(signed int *array, int length)
+{
+    for (int i = 0; i < length; i++)
         *(array + i) = 32767 - i;
 }
 
-void copyArray(int *source_array, int *destination_array, int length) {
-    for(int i = 0; i < length; i++) {
+void copyArray(int *source_array, int *destination_array, int length)
+{
+    for (int i = 0; i < length; i++)
+    {
         *(destination_array + i) = *(source_array + i);
     }
 }
--- array_functions.h
+++ array_functions.h
@@ -2,7 +2,7 @@
 #define ARRAY_FUNCTIONS_H
 int validateSortedArray(int *input_array, int length, int ascending);
 void printArray(int *input_array, int length);
-int randomizeArray(signed int *array, int length);
+void randomizeArray(signed int *array, int length);
 void fillArrayAscening(signed int *array, int length);
 void fillArrayDescending(signed int *array, int length);
 void copyArray(int *source_array, int *destination_array, int length);
