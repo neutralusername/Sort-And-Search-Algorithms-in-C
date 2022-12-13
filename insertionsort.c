2eed7c7a7eef4a5a661ab45d915bd03ad8e8e327
fabian <fabian@fabian.at>
2022-12-13 08:47:24 +0100

    fixed compile error

--- insertionsort.c
+++ insertionsort.c
@@ -1,31 +1,16 @@
 #include "node.h"
 #include <stdio.h>
 
-struct Node *insertionSortLinkedList(struct Node *head, int ascending) 
+struct Node *sortedInsert(struct Node *head, struct Node *insertionNode, int ascending) //parameters: the current head of the list, the node to be inserted, and a boolean indicating whether the list should be sorted in ascending or descending order
 {
-    struct Node *result = NULL; // the result list is initially empty
-    struct Node *current = head; // the current element is the first element in the original list
-    struct Node *next; // the next element is used to save the next element in the original list
-    while (current != NULL)  // go over every element in the list
-    {
-        next = current->next; // save the next element
-        result = sortedInsert(result, current, ascending); // insert the current element at the correct position in the result list.
-        current = next; // set the current element to the next element
-    }
-    return result;
-}
-
-struct Node *sortedInsert(struct Node *head, struct Node *insertionNode, int ascending) //parameters: the head of the list, the node to be inserted, and a boolean indicating whether the list should be sorted in ascending or descending order
-{
-    struct Node *current; 
-    if (head == NULL || (ascending ? head->data >= insertionNode->data : head->data <= insertionNode->data)) // if the list is empty or the node should be inserted at the beginning of the list
-    {
+    if (head == NULL || (ascending ? head->data >= insertionNode->data : head->data <= insertionNode->data)) {
+        // if the list is empty or the node should be inserted at the beginning of the new list
         insertionNode->next = head; // set the next element of the node to be inserted to the current head of the list
         head = insertionNode; // set the head of the list to the node to be inserted
     } 
     else  // if the node should be inserted somewhere else in the list
     {
-        current = head; // set the current element to the head of the list
+         struct Node *current = head; // set the current element to the head of the list
         while (current->next != NULL && (ascending ? current->next->data < insertionNode->data : current->next->data > insertionNode->data)) // iterate through the list until the next element is larger than the node to be inserted
         {
             current = current->next; // set the current element to the next element
@@ -34,6 +19,20 @@ struct Node *sortedInsert(struct Node *head, struct Node *insertionNode, int asc
         current->next = insertionNode; // set the next element of the current element to the node to be inserted
     }
     return head; // return the head of the list
+}
+
+struct Node *insertionSortLinkedList(struct Node *head, int ascending) 
+{
+    struct Node *result = NULL; // the result list is initially empty.
+    struct Node *current = head; // the current element is the first element in the original list. 
+    struct Node *next; // the next element is used to save the next element in the original list
+    while (current != NULL)  // go over every element in the list
+    {
+        next = current->next; // save the next element
+        result = sortedInsert(result, current, ascending); // insert the current element at the correct position. result is the new head of the new list.
+        current = next; // set the current element to the next element
+    }
+    return result;
 }
 
 
--- insertionsort.h
+++ insertionsort.h
@@ -1,5 +1,6 @@
 #ifndef INSERTIONSORT_H
 #define INSERTIONSORT_H
 struct Node *insertionSortLinkedList(struct Node *head, int ascending);
+struct Node *sortedInsert(struct Node *head, struct Node *insertionNode, int ascending);
 void insertionSortArray(int *array, int length, int ascending);
 #endif
