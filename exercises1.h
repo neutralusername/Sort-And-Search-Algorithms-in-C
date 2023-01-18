ee6383c1396ee904fb2fa5a2adce2851c09f56ae
fabian <fabian@fabian.at>
2023-01-18 08:43:25 +0100

    fixed .h

--- exercises1.h
+++ exercises1.h
@@ -1,9 +1,5 @@
 #ifndef EXERCISES1_H
 #define EXERCISES1_H
-void execute_1_1();
-void execute_1_2();
-void execute_1_3();
-void execute_1_4();
 void execute_2_1();
 void execute_2_2();
 void execute_2_3();
